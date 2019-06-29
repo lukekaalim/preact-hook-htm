ab7efdf9683448e6bda71cefb8922a9c5068ae9c
Luke Kaalim <lukekaalim@gmail.com>
2019-06-30 00:03:22 +1000

    Add flow settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import flowEntry from 'rollup-plugin-flow-entry'
 
 const rollupConfig = {
   input: 'src/index.js',
@@ -8,7 +9,7 @@ const rollupConfig = {
     { file: 'dist/preact-hook-htm.cjs.js', format: 'cjs' },
     { file: 'dist/preact-hook-htm.umd.js', format: 'umd', name: 'PREACT_HOOK_HTM' },
   ],
-  plugins: [resolve(), terser()],
+  plugins: [resolve(), terser(), flowEntry()],
 };
 
 export default rollupConfig;
